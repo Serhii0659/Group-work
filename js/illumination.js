8b1488c0c8ce61150832a6756b481555210b81c1
uddorn <tkachenkoff91@gmail.com>
2024-07-16 06:05:58 +0300

    Доповнення головної сторінки №2
    прибрав непотрібну частину кода в illumination.js для полегшення його функціоналу та щоби працювала на всіх новостворених сторінках

--- js/illumination.js
+++ js/illumination.js
@@ -1,27 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const leftButton = document.querySelector('.left-button');
-    const rightButton = document.querySelector('.right-button');
-    const menuList = document.querySelector('.menu-list');
     const navLinks = document.querySelectorAll('.nav-list__link');
     const navIndicator = document.querySelector('.nav-indicator');
-
-    if (leftButton && rightButton && menuList) {
-        leftButton.addEventListener('click', () => {
-            menuList.scrollBy({ left: -300, behavior: 'smooth' });
-        });
-
-        rightButton.addEventListener('click', () => {
-            menuList.scrollBy({ left: 300, behavior: 'smooth' });
-        });
-
-        document.addEventListener('keydown', (event) => {
-            if (event.key === 'ArrowLeft') {
-                menuList.scrollBy({ left: -300, behavior: 'smooth' });
-            } else if (event.key === 'ArrowRight') {
-                menuList.scrollBy({ left: 300, behavior: 'smooth' });
-            }
-        });
-    }
 
     if (navLinks && navIndicator) {
         navLinks.forEach(link => {
